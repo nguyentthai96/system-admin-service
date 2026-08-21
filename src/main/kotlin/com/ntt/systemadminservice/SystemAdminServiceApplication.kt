fee0b95893cde1df8a3fcf01873e08c108295959
nguyentthai96 <nguyentthai96@gmail.com>
2026-08-21 16:24:25 +0700

    fix(system-admin): enable DataProperties configuration properties

--- src/main/kotlin/com/ntt/systemadminservice/SystemAdminServiceApplication.kt
+++ src/main/kotlin/com/ntt/systemadminservice/SystemAdminServiceApplication.kt
@@ -1,9 +1,12 @@
 package com.ntt.systemadminservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import com.ntt.basecore.autoconfigure.data.DataProperties
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataProperties::class)
 class SystemAdminServiceApplication
 
 fun main(args: Array<String>) {
